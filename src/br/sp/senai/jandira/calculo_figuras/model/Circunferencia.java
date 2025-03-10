4f60bb1257825c2869c7389318350c209a98b1fa
SAMARA ROLIM DOS SANTOS <25132563@CFP127ALUNO.LOCAL>
2025-03-10 11:20:02 -0300

    inicio do projeto de calculo das figuras

---
+++ src/br/sp/senai/jandira/calculo_figuras/model/Circunferencia.java
@@ -0,0 +1,20 @@
+package br.sp.senai.jandira.calculo_figuras.model;
+
+public class Circunferencia {
+	
+	private double raio;
+	
+	public void setRaio (double raio) {
+		this.raio = raio;
+	}
+	
+	public double getRaio() {
+		return this.raio;	
+	}
+	
+	public double calcularArea() {
+		double area = Math.PI * Math.pow(raio, 2);
+		return area;
+	}
+}
+
---
+++ src/br/sp/senai/jandira/calculo_figuras/model/Retangulo.java
@@ -0,0 +1,17 @@
+package br.sp.senai.jandira.calculo_figuras.model;
+
+public class Retangulo {
+
+	private double base;
+	private double altura;
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double
+}
